3e62272d1727dff60afb97f28ad372963a39b233
Alexis Payen de la Garanderie <alcazar@freeforall.fr>
2017-02-16 21:23:24 +0530

    Improved word page

--- OtakuAssistant/Code/XamlConverters.cs
+++ OtakuAssistant/Code/XamlConverters.cs
@@ -11,14 +11,14 @@ using OtakuLib;
 
 namespace OtakuAssistant
 {
-    public class SearchViewNameSize : IValueConverter
+    public class SearchViewHanziSize : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             string str = value as string;
             if (str == null)
             {
-                return 40;
+                return 0;
             }
             else if (str.Length <= 3)
             {
@@ -48,7 +48,7 @@ namespace OtakuAssistant
         }
     }
     
-    public class SearchViewNameWrapper : IValueConverter
+    public class SearchViewHanziWrapper : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
@@ -58,6 +58,66 @@ namespace OtakuAssistant
                 return str;
             }
             else if (str.Length <= 10)
+            {
+                int sep = (str.Length + 1)/2;
+                return string.Format("{0}\n{1}", str.Substring(0, sep), str.Substring(sep));
+            }
+            else
+            {
+                int sep = (str.Length + 2)/3;
+                return string.Format("{0}\n{1}\n{2}", str.Substring(0, sep), str.Substring(sep, sep), str.Substring(2*sep));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+
+    public class WordViewHanziSize : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string str = value as string;
+            if (str == null)
+            {
+                return 0;
+            }
+            else if (str.Length <= 6)
+            {
+                return 48;
+            }
+            else if (str.Length <= 12)
+            {
+                return 40;
+            }
+            else if (str.Length <= 20)
+            {
+                return 32;
+            }
+            else
+            {
+                return 24;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+    
+    public class WordViewHanziWrapper : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string str = value as string;
+            if (str.Length <= 5)
+            {
+                return str;
+            }
+            else if (str.Length <= 16)
             {
                 int sep = (str.Length + 1)/2;
                 return string.Format("{0}\n{1}", str.Substring(0, sep), str.Substring(sep));
